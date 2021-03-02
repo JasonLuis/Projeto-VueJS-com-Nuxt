4db022431ced1c25e1fafc603f2071d24ca1a2b8
JasonLuis <jason20luis@gmail.com>
2021-03-02 20:16:22 -0300

    pegando dados de uma API utilizando Axios

--- components/Comments.vue
+++ components/Comments.vue
@@ -1,67 +1,49 @@
 <template>
   <v-row justify="center" align="center">
-    <FormTodo
-    @add-todo="addComment"
-    >
-    </FormTodo>
+    <FormTodo @add-todo="addComment" />
     <v-container>
-      <v-data-table
-      :headers="headers"
-        :items="comment"
-        class="elevation-1"
-      >
-    <template #[`item.name`]="{value}">
-      {{value}}
-    </template>
-    <template #[`item.comment`]="{value}">
-      {{value}}
-    </template >
-    <template #[`item.action`]="{item }">
-      <v-icon
-        small
-        class="mr-2"
-        @click="deleteItem(item)"
-      >
-       mdi-delete
-      </v-icon>
-    </template>
-  </v-data-table>
-  </v-container>
+      <v-data-table :headers="headers" :items="comment" class="elevation-1">
+        <template #[`item.name`]="{ value }"> {{ value }}</template>
+        <template #[`item.comment`]="{ value }">{{ value }} </template>
+        <template #[`item.action`]="{ item }">
+          <v-icon small class="mr-2" @click="deleteItem(item)">
+            mdi-delete
+          </v-icon>
+        </template>
+      </v-data-table>
+    </v-container>
   </v-row>
 </template>
 
 <script>
-import FormTodo from "./FormTodo.vue";
+import FormTodo from './FormTodo.vue'
 
 export default {
-    components:{
-      FormTodo,
+  components: {
+    FormTodo,
+  },
+  data() {
+    return {
+      headers: [
+        {
+          text: 'name',
+          align: 'start',
+          sortable: false,
+          value: 'name',
+        },
+        { text: 'comment', value: 'comment' },
+        { text: 'action', value: 'action' },
+      ],
+      comment: [],
+    }
+  },
+  methods: {
+    addComment(comments) {
+      this.comment.push(comments)
     },
-    data() {
-      return{
-        headers: [
-          {
-            text: 'name',
-            align: 'start',
-            sortable: false,
-            value: 'name',
-          },
-          { text: 'comment', value: 'comment' },
-          { text: 'action', value: 'action' },
-        ],
-        comment: [
-          
-        ],
-      }
+    deleteItem(item) {
+      this.comment.splice(item, 1)
     },
-    methods: {
-      addComment(comments){
-        console.log(comments);
-        this.comment.push(comments);
-      },
-      deleteItem(item){
-        this.comment.splice(item, 1);
-      }
-    },
-  }
+  },
+}
 </script>
--- components/FormTodo.vue
+++ components/FormTodo.vue
@@ -1,98 +1,92 @@
 <template>
-<v-container >
-  <form>
-    <v-text-field 
-      v-model="name"
-      :error-messages="nameErrors"
-      :counter="20"
-      filled
-      label="Nome"
-      required
-      @input="$v.name.$touch()"
-      @blur="$v.name.$touch()"
-    ></v-text-field>
-
-    <v-textarea
-      v-model="comment"
-      :error-messages="commentErrors"
-      :counter="50"
-      name="input-7-1"
-      filled
-      label="Comentarios"
-      auto-grow
-      required
-       @input="$v.comment.$touch()"
-      @blur="$v.comment.$touch()"
-    ></v-textarea>
-    <v-btn
-      class="mr-4"
-      @click="submit"
-    >
-      submit
-    </v-btn>
-    <v-btn @click="clear">
-      Limpar
-    </v-btn>
-  </form>
-</v-container>
-  
+  <v-container>
+    <form>
+      <v-text-field
+        v-model="name"
+        :error-messages="nameErrors"
+        :counter="20"
+        filled
+        label="Nome"
+        equired@input="$v.name.$touch()"
+        @blur="$v.name.$touch()"
+      >
+      </v-text-field>
+      <v-textarea
+        v-model="preco"
+        :error-messages="precoErrors"
+        :counter="50"
+        name="input-7-1"
+        filled
+        label="Preço"
+        auto-grow
+        required@input="$v.preco.$touch()"
+        @blur="$v.preco.$touch()"
+      >
+      </v-textarea>
+      <v-btn class="mr-4" @click="submit">submit</v-btn>
+      <v-btn @click="clear">Limpar</v-btn>
+    </form>
+  </v-container>
 </template>
 
 <script>
-  import { validationMixin } from 'vuelidate'
-  import { required, maxLength } from 'vuelidate/lib/validators'
+import { validationMixin } from 'vuelidate'
+import { required, maxLength } from 'vuelidate/lib/validators'
 
-  export default {
-    mixins: [validationMixin],
+export default {
+  mixins: [validationMixin],
 
-    validations: {
-      name: { required, maxLength: maxLength(20) },
-      comment: { required, maxLength: maxLength(50) }
+  validations: {
+    name: { required, maxLength: maxLength(20) },
+    preco: { required, maxLength: maxLength(50) },
+  },
+
+  data: () => ({
+    name: undefined,
+    preco: undefined,
+  }),
+
+  computed: {
+    nameErrors() {
+      const errors = []
+      if (!this.$v.name.$dirty) return errors
+      !this.$v.name.maxLength &&
+        errors.push('Nome não pode ter mais de 20 caracteres')
+      !this.$v.name.required && errors.push('Nome é obrigatorio.')
+      return errors
     },
-
-    data: () => ({
-      name: undefined,
-      comment: undefined,
-    }),
-
-    computed: {
-      nameErrors () {
-        const errors = []
-        if (!this.$v.name.$dirty) return errors
-        !this.$v.name.maxLength && errors.push('Nome não pode ter mais de 20 caracteres')
-        !this.$v.name.required && errors.push('Nome é obrigatorio.')
-        return errors
-      },
-      commentErrors() {
-        const errors = []
-        if (!this.$v.comment.$dirty) return errors
-        !this.$v.comment.maxLength && errors.push('Um comentario não pode ter mais de 50 caracteres')
-        !this.$v.comment.required && errors.push('Comentario é obrigatorio.')
-        return errors
-      },
+    precoErrors() {
+      const errors = []
+      if (!this.$v.preco.$dirty) return errors
+      !this.$v.preco.maxLength &&
+        errors.push('Um comentario não pode ter mais de 50 caracteres')
+      !this.$v.preco.required && errors.push('Comentario é obrigatorio.')
+      return errors
     },
+  },
 
-    methods: {
-      submit () {
-        
-        if(this.name.trim() === '' || this.comment.trim() === ''){
-          return 
-        }else{
-          this.$emit('add-todo', {
-            name: this.name,
-            comment: this.comment,
-          });
-          this.clear();
-        }
-
-        
-      },
-      clear () {
-        this.$v.$reset()
-        this.name = ''
-        this.comment = ''
-      },
+  methods: {
+    submit() {
+      this.$v.$touch()
+      if (
+        this.name === undefined ||
+        this.name === '' ||
+        this.preco === undefined ||
+        this.preco === ''
+      ) {
+        return
+      }
+      this.$emit('add-todo', {
+        name: this.name,
+        preco: this.preco,
+      })
+      this.clear()
     },
-  }
+    clear() {
+      this.$v.$reset()
+      this.name = ''
+      this.preco = ''
+    },
+  },
+}
 </script>
-
