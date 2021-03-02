71bb06f3a07323dfec74298851c95e4e30a66f00
Djeison Luis <jason20luis@gmail.com>
2021-03-02 16:39:50 -0300

    Projeto com NuxtJS e Vuetify

---
+++ pages/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-main >
+      <div class="container">
+        <v-row>
+          <h1>Cadastro</h1>
+        </v-row>
+        <v-row>
+          <comments />
+        </v-row>
+      </div>
+    </v-main>
+  </v-row>
+</template>
+
+<script>
+
+import Comments from "~/components/Comments.vue";
+
+export default {
+  
+  components: {
+    Comments,
+  },
+
+}
+</script>
+
+<style scoped>
+  .container{
+   max-width: 1200px;
+  }
+</style>
